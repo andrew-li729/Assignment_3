2761d6d28f04e503cb1d4c5b2701fff058e390a6
Andrew <andrew.li729@gmail.com>
2019-11-15 14:36:05 -0600

    Grocery class functional and displaying in main class + grand total for grocery items

--- src/demo.java
+++ src/demo.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class demo {
     public static void main(String[]args){
+        double grandTotal;
 
         Grocery bread=new Grocery(11,"Bread",1.50,2,0);
         bread.Display();
         bread.displayGrocery();
+        System.out.println();
+        Grocery apple=new Grocery(12,"Apple",0.75,6,1);
+        apple.Display();
+        apple.displayGrocery();
+
+        grandTotal=apple.computeTotalPrice()+bread.computeTotalPrice();
+        System.out.println("Grand total: $"+grandTotal);
 
     }
 }
